b2d54a2c3376cef4ef41e7173895357fb09c317b
bsoper <soper103@gmail.com>
2018-01-17 22:51:24 -0500

    Added an application skeleton.

---
+++ src/Application.py
@@ -0,0 +1,46 @@
+from Expenditure import Expenditure
+from Income import Income
+
+class Application:
+	expenditures = []
+	total_expenditure = 0.0
+
+	incomes = []
+	total_monthly_income = 0.0
+
+	current_savings = 0.0
+	daily_allowance = 0.0
+
+	def printExpenditures(self):
+		print self.expenditures
+
+	def printIncomes(self):
+		print self.incomes
+
+	def printInformation(self):
+		self.printExpenditures()
+		self.printIncomes()
+
+
+	def __init__(self):
+		return
+
+	def addExpenditure(exp):
+		return
+
+	def addIncome(inc):
+		return
+
+	def calculateDailyAllowance():
+		return
+
+
+
+app = Application()
+
+app.expenditures.append(Expenditure('rent', 1000.0, 0))
+app.expenditures.append(Expenditure('car', 200.0, 700))
+
+app.incomes.append(Income('work', 3000.0, 0))
+
+app.printInformation()
