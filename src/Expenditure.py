fca9f7bb4dd41b9eb0948605258dfda63f2de59c
bsoper <soper103@gmail.com>
2018-01-17 22:49:26 -0500

    Added Income class and renamed Expenditure. Added time frames.

---
+++ src/Expenditure.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+from Enum import TimeFrames
+
+class Expenditure:
+	Label = ''
+	amount = 0.0
+	remaining_payments = 0
+	time_frame = TimeFrames.MONTH
+
+	def __init__(self, label, amount, remaining_payments):
+		self.label = label
+		self.amount = amount
+		self.remaining_payments = remaining_payments
+
+	def __repr__(self):
+		return '(Expenditure: ' + ', '.join([self.label, str(self.amount), str(self.remaining_payments), str(self.time_frame)]) + ')'
--- src/MonthlyExpenditure.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-
-class MonthlyExpenditure:
-	str Label
-	float amount
-	int remaining_payments
